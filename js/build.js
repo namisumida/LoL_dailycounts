adfadce427b3f0713449fb6003a1a060a7cc1b20
namisumida <sumida.nami@gmail.com>
2019-01-02 11:20:58 -0500

    Remove dots

--- js/build.js
+++ js/build.js
@@ -96,72 +96,6 @@ function setup() {
              .attr("id", "line420")
              .style("stroke", "none")
              .attr("d", line); // calls line generator
-
-  // Draw dots
-  var group_dots = svg.append("g")
-                      .attr("id", "group_dots");
-  group_dots.selectAll("dot")
-            .data(dataset)
-            .enter()
-            .append("circle")
-            .attr("class", "dot")
-            .attr("cx", function(d) {
-              return xScale(d.day);
-            })
-            .attr("cy", function(d) {
-              return yScale(d.ngames);
-            })
-            .attr("r", 4)
-            .style("fill", function(d) {
-              if (d.queueid==1200) {
-                return color1200;
-              }
-              else { return "none"; }
-            });
-
-  // Mouseover dots
-  // On mouseovers of points
-  svg.selectAll(".dot").on("mouseover", function(d) {
-
-    currentDate = d.day;
-    var xPosition = parseFloat(d3.select(this).attr("cx"));
-
-    // Change dot styles
-    svg.selectAll(".dot")
-        .attr("r", function(d) {
-          if (d.day==currentDate) {
-            return 5;
-          }
-          else { return 4; }
-        });
-
-    // Show date
-    xAxis.ticks(1).tickValues([currentDate]) //specify an array here for values
-    svg.selectAll(".xAxis").call(xAxis)
-       .selectAll(".tick text")
-       .style("text-anchor", function(d) {
-         if (xPosition < 20) {
-           return "start";
-         }
-         else if (xPosition > w_svg-30) {
-           return "end";
-         }
-         else { return "middle"; }
-       }) // end text anchor
-  }) // end on mouseover
-  svg.selectAll(".dot").on("mouseout", function(d) {
-    svg.selectAll(".dot").attr("r", 4); // change dot style back
-
-    // Change xAxis back
-    if (show420 | show450) {
-      xAxis.tickValues(xTickValues400s);
-      svg.select(".xAxis").call(xAxis);
-    }
-    else {
-      xAxis.tickValues(xTickValues1200);
-      svg.select(".xAxis").call(xAxis);
-    }
-  });
 }; // end setup function
 
 function resize() {
@@ -209,12 +143,6 @@ function resize() {
   svg.select("#group_lines")
      .select("#line420")
      .attr("d", line); // calls line generator
-
-  // Draw dots
-  svg.select("#group_dots").selectAll(".dot")
-      .attr("cx", function(d) {
-        return xScale(d.day);
-      });
 }; // end resize function
 
 function init() {
@@ -258,41 +186,11 @@ d3.csv('data/daily_play_counts.csv', rowConverter, function(data) {
            .attr("d", line);
         svg.select("#line420") // redefine even if we don't show it
            .attr("d", line);
-        svg.selectAll(".dot")
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else if (d.queueid == 450) {
-               return color450;
-             }
-             else { return "none"; }
-           });
 
      } // end if 420 not shown
      else { // if 420 is already shown
        svg.select("#line450") // just show the line (that should have been updated when 420 was changed)
           .style("stroke", color450);
-       svg.selectAll(".dot")
-          .transition()
-          .duration(500)
-          .attr("cx", function(d) {
-            return xScale(d.day);
-          })
-          .style("fill", function(d) {
-            if (d.queueid == 1200) {
-              return color1200;
-            }
-            else if (d.queueid == 450) {
-              return color450;
-            }
-            else { return color420; }
-          })
      }
       // change button color
       d3.select(this).style("background-color", color450)
@@ -313,38 +211,11 @@ d3.csv('data/daily_play_counts.csv', rowConverter, function(data) {
         svg.select("#line420")
            .attr("d", line)
            .style("stroke", "none");
-        svg.selectAll(".dot")
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else { return "none"; }
-           });
       } // end if 420 line is also hidden
       else { // else 420 line is still there
         svg.select("#line450") // hide line
            .attr("d", line)
            .style("stroke", "none");
-        svg.selectAll(".dot") // hide dots
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else if (d.queueid == 420) {
-               return color420;
-             }
-             else { return "none"; }
-           })
       }; // end else 420 line still there
       // change button color
       d3.select(this).style("background-color", "white")
@@ -375,40 +246,10 @@ d3.csv('data/daily_play_counts.csv', rowConverter, function(data) {
            .attr("d", line);
         svg.select("#line450") // redefine even if we don't show it
            .attr("d", line);
-        svg.selectAll(".dot")
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else if (d.queueid == 420) {
-               return color420;
-             }
-             else { return "none"; }
-           })
      } // end if 450 not shown
      else { // 450 is already shown, then don't want transition
         svg.select("#line420") // just show the line (that should have been updated when 450 was changed)
            .style("stroke", color420);
-        svg.selectAll(".dot")
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else if (d.queueid == 420) {
-               return color420;
-             }
-             else { return color450; }
-           })
      }
       // change button color
       d3.select(this).style("background-color", color420)
@@ -429,38 +270,11 @@ d3.csv('data/daily_play_counts.csv', rowConverter, function(data) {
         svg.select("#line450")
            .attr("d", line)
            .style("stroke", "none");
-        svg.selectAll(".dot")
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else { return "none"; }
-           })
       } // end if 450 is also hidden
       else { // 450 still there
         svg.select("#line420") // hide line
            .attr("d", line)
            .style("stroke", "none");
-        svg.selectAll(".dot")
-           .transition()
-           .duration(500)
-           .attr("cx", function(d) {
-             return xScale(d.day);
-           })
-           .style("fill", function(d) {
-             if (d.queueid == 1200) {
-               return color1200;
-             }
-             else if (d.queueid == 450) {
-               return color450;
-             }
-             else { return "none"; }
-           })
       }; // end if 450 still there
       d3.select(this).style("background-color", "white") // change button color back
                      .style("color", "#a19da8");
